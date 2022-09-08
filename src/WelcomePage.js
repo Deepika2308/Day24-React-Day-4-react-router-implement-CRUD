6d62f13f7d8ccec6f486050e05e8fd0e0f854f01
Deepika2308 <deepika.vijai@gmail.com>
2022-09-08 22:41:01 +0530

    added links to create and list users

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -1,11 +1,14 @@
 import {Link} from "react-router-dom";
+import "./styles.css";
 
 export function WelcomePage(){
     return(
         <div>
             <h1>Welcome!!</h1>
-            <div>
-                <Link to="/create-user">Create User</Link>
+            <div className="d-flex flex-column gap-2 my-5">
+                <div>Get started with below links</div>
+                <Link to="/create-user" className="start-link fw-bold">Create User</Link>
+                <div>Click <Link to="/users" className="start-link fw-bold">here</Link> to list,edit or delete users(s)</div>
             </div>
         </div>
     )
